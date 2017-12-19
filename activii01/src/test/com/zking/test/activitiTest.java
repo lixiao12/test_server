4670b4c6ba84fb969a24f6039eb948f66a641a4a
lixiao12 <2949424758@qq.com>
2017-12-19 22:43:00 +0800

    ssss

--- activii01/src/test/com/zking/test/activitiTest.java
+++ activii01/src/test/com/zking/test/activitiTest.java
@@ -19,6 +19,7 @@ public class activitiTest {
     public void testCreateTable(){
         processEngine = ProcessEngines.getDefaultProcessEngine();
         System.out.println("创表成功");
+        System.out.println("ssss");
     }
 
     @Test
